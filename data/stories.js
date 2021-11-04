2e10eea60919f405714b42bf948aa736c0359d70
James Wallace <4538317+james-wallace-nz@users.noreply.github.com>
2021-11-05 10:44:00 +1300

    Add stories

---
+++ data/stories.js
@@ -0,0 +1,33 @@
+export default = [
+  { 
+    title: "Pizza",
+    story: "Pizza was invented by a {{adjective}} {{nationality}} chef titled {{person}}. To make a pizza, you need to take a lump of {{noun}}, and make a think, round {{adjective}} {{noun}}. Then you cover it with {{adjective}} sauce, {{adjective}} cheese, and fresh chopped {{plural noun}}. Next you have to bake it in a very hot {{noun}}. When it is done, cut it into {{number}} {{shapes}}. Some people like {{food}} pizza the best, but my favourite is the {{food}} pizza. If I could, I would eat pizza {{number}} times a day!",
+
+  }
+
+  title: "Snowman"
+  story: "Let's build a snowman! First we need to have a really {{adjective}} snowstorm, of course on a {{adjective}} day. Watching all that {{color}} snow fall makes make {{verb}} for a bowl of {{adjective}} {{vegetable}}! {{Exclamation}} it's still {{verb ending in -ing}} and there's a lot of it! Let's go! Next is {{verb ending in -ing}} to go out in the cold. Once that is done, we need to get busy. We make a {{noun}} and {{verb}} it in the {{place}}. It gets so {{adj}} we can't roll it anymore. Then we do {{number}} more and stack them on top of each other. We {{verb}} for {{noun}} in the driveway for the {{noun}}, eyes and {{noun}}. Mom gives us a scarf, hat and {{vegetable}} to complete Mr. {{person}}! {{exclamation}} he's done! Finally. Now the best part, {{adjective}} {{noun}}."
+
+  {
+    title: "Halloween",
+    story: "Last night I went to the most {{adjective one}} Halloween party! The invitation was delivered by {{animal}} and told me to go to the deep dark {{place}} and {{verb}} all the way to the very top of the spooky {{noun}}. I {{verb past tense}} the doorbell {{Adverb}}. {{Exclamation}}! My friend {{friend's name}} answered the door dressed up as {{celebrity}} and said \"{{silly word}}!\" there were {{number}} different costumes, including {{adjective two}} ghouls and mummified {{noun plural}}. My favourite part of the party was the pumpkin {{verb ending in ing}} and bobbing for {{noun plural}}. I can't wait for next Halloween - it will be even more {{adjective three}}!",
+    required_words: [
+      "adjective one",
+      "adjective two",
+      "adjective three", 
+      "animal", 
+      "place", 
+      "verb", 
+      "noun", 
+      "verb", 
+      "past tense", 
+      "adverb", 
+      "exlamation", 
+      "friend's name", 
+      "celebrity", 
+      "silly word", 
+      "number", 
+      "noun plural", 
+      "very ending in ing"
+    ]
+]
