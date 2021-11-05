4dbc5ff57945b3429316f32e236dae0a5c433aff
ppc99999 <ppc99999@gmail.com>
2021-11-05 15:05:28 +1300

    form input/put to new story

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Route } from 'react-router-dom'
 
 import Nav from './Nav'
 import About from './About'
 import Story from './Story'
-import AddWord from './AddWords'
+import AddWords from './AddWords'
 import NewStory from './NewStory'
 
 import storyData from '../../data/stories'
 
+const initial =
+  { noun: '', adjective: '', nationality: '', plural_noun: '', person: '', shapes: '', food: '', number: '' }
+
 function App () {
+  const [words, setWords] = useState(initial)
+
   return (
     <>
       <div className='title'>
@@ -21,8 +26,9 @@ function App () {
         <div className='nav'><Route path='/' render={() => <Nav stories={storyData} />} /></div>
         <div className='home'>
           <Route path='/' exact component={About} />
-          <Route path='/new-story' exact component={NewStory} />
-          <Route path='/add-words' exact component={AddWord} />
+          <Route path='/new-story' exact render={() => <NewStory words={words} />} />
+          <Route path='/add-words' exact
+            render={() => <AddWords words={words} setWords={setWords} />}/>
           <Route path='/story/:storyTitle' exact render={() => <Story stories={storyData} />} />
         </div>
       </div>
